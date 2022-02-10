16b9d5faab0335fe228655b6c8716cd9a1fb9a10
linarano <ranothing@gmail.com>
2022-02-10 18:33:10 +0900

    .

---
+++ cal-net/app/src/main/java/cal/net/CalculatorClient.java
@@ -0,0 +1,10 @@
+package cal.net;
+
+public class CalculatorClient {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ cal-net/app/src/main/java/cal/net/CalculatorServer.java
@@ -0,0 +1,10 @@
+package cal.net;
+
+public class CalculatorServer {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
